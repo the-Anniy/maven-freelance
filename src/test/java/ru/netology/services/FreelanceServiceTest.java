13a149f6d2ac86339af0ce9beb24a0e7c45cec15
Anna Paliy <annpaliy2015@gmail.com>
2024-11-15 13:43:13 +0300

    Parametrized Test

--- src/test/java/ru/netology/services/FreelanceServiceTest.java
+++ src/test/java/ru/netology/services/FreelanceServiceTest.java
@@ -1,31 +1,17 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class FreelanceServiceTest {
 
-    @Test
-    public void shouldCalculateCaseOne() {
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/test_data.csv")
+    public void shouldCalculateParamTest(int income, int expenses, int threshold, int expected) {
         FreelanceService service = new FreelanceService();
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
 
-        int expected = 3;
-        int actual = service.calculate(income, expenses, threshold);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalculateCaseTwo() {
-        FreelanceService service = new FreelanceService();
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-
-        int expected = 2;
         int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
